0fd6c3817e8b9bfd725921e8fcf6f8639a57ea2f
Druidan <44009123+Druidan@users.noreply.github.com>
2019-05-11 11:53:52 -0500

    Updated the API routes. I have a prototype of the put route and I got rid of some of the uneeded routes.

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -1,115 +1,97 @@
-// *********************************************************************************
-// api-routes.js - this file offers a set of routes for displaying and saving data to the db
-// *********************************************************************************
-
-// Dependencies
-// =============================================================
-
 // Requiring our model
 const db = require('../models');
 
 // Routes
 // =============================================================
 
-module.exports = function(app) {
+module.exports = function (app) {
 
   //GET route for all burger data
   app.get('/api/burgerRoster/', (req, res) => {
     db.Burgers.findAll({
-      include: [
-        {
-          model: db.Condiments,
-          as: 'Condiments',
-          required: false,
-          attributes: ['id', 'name'],
-          through: { attributes: [] }
-        },
-        {
-          model: db.Breads,
-          as: 'Breads',
-          required: false,
-          attributes: ['id', 'name'],
-          through: { attributes: [] }
-        },
-        {
-          model: db.Meats,
-          as: 'Meats',
-          required: false,
-          attributes: ['id', 'name'],
-          through: { attributes: [] }
-        },
-      ],
-    })
+        include: [{
+            model: db.Condiments,
+            as: 'Condiments',
+            required: false,
+            attributes: ['id', 'name'],
+            through: {
+              attributes: []
+            }
+          },
+          {
+            model: db.Breads,
+            as: 'Breads',
+            required: false,
+            attributes: ['id', 'name'],
+            through: {
+              attributes: []
+            }
+          },
+          {
+            model: db.Meats,
+            as: 'Meats',
+            required: false,
+            attributes: ['id', 'name'],
+            through: {
+              attributes: []
+            }
+          },
+        ],
+      })
       .then((burgerData) => {
         res.json(burgerData);
-      })
-  })
-
-  //GET route for all meat data
-  app.get('/api/burgerRoster/meatData', (req, res) => {
-    db.burgerMeaties.findAll({})
-      .then((meatData) => {
-        res.json(meatData);
       })
   })
 
   // GET route for a specific burger.
   app.get('/api/burgerRoster/:id', (req, res) => {
     db.Burgers.findAll({
-      include: [{
-        model: models.Condiments,
-        as: 'condiments',
-        required: false,
-        attributes: ['id', 'name'],
-        through: { attributes: [] }
-      }],
-      where: { 
-        id: req.params.id,
-      },
-    })
+        where: {
+          id: req.params.id,
+        },
+      })
       .then((burgerData) => {
         res.json(burgerData);
       });
   });
   // ------------------
 
-  // POST route
-  app.post('/api/WhateverWeNameIt', (req, res) => {
-    db.WhateverWeNameIt.create({
-      // stuff goes here
-    })
-      .then((data) => {
-        res.json(data);
-      });
-  });
-  // ------------------
-
   // PUT route
-  app.put('/api/WhateverWeNameIt', (req, res) => {
-    db.WhateverWeNameIt.update(req.body,
-      {
-        where: {
-          id: req.body.id,
-        },
+  app.put('/api/burgerRoster/', (req, res) => { // When the update request is sent to the api...
+    // We are recieving an array of key value pairs with an id and an integer to add to the previous burger score.
+    const burgerArr = req.body; 
+
+    // We take our recieved arrar and make a get call and then update call on each burger.
+    burgerArr.foreach(burger => {
+
+      const addToScore = burger.score; // We identify the burger's id with a variable.
+      const burgerURL = `/api/burgerRoster/${burger.id}` // We establish a URL for our GET call using the burger id.
+
+      $.ajax({ // We make a call to our API for a specific burger.
+        url: burgerURL,
+        method: "GET"
+      }).then(response => {
+
+        //We establish the burger's original score
+        const ogBurgerScore = response.score;
+        const newBurgerScore = ogBurgerScore + addToScore; // We create a new score by adding the integer we recieved.
+        burger.score = newBurgerScore; // We declare the new score value as part of the burger.
+
+        // We make the update call to the database for that specific burger using its ID.
+        db.Burgers.update(burger.score, { 
+            where: {
+              id: burger.id,
+            },
+          })
+          .then((upData) => {
+            res.json(upData);
+          });
       })
-      .then((data) => {
-        res.json(data);
-      });
+
+    });
   });
   // ------------------
 
-  // DELETE route
-  app.delete('/api/WhateverWeNameIt/:id', (req, res) => {
-    db.WhateverWeNameIt.destroy({
-      where: {
-        id: req.params.id,
-      },
-    })
-      .then((data) => {
-        res.json(data);
-      });
-  });
-  // ------------------
 };
 
 // *********************************************************************************
